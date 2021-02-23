23c86d9f4dd01587ebdc036d4717fcadd8f8d679
Anže Pureber <anze.pureber@infinum.com>
2021-02-23 23:34:20 +0100

    add prettier, chakra, refactor directory structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 module.exports = {
-  siteMetadata: {
-    title: "anzep-gatsby",
-  },
-  plugins: ["gatsby-plugin-emotion"],
+	siteMetadata: {
+		title: 'anzep-gatsby',
+	},
+	plugins: [
+		'gatsby-plugin-emotion',
+		{
+			resolve: '@chakra-ui/gatsby-plugin',
+			options: {
+				isResettingCSS: true,
+				isUsingColorMode: true,
+			},
+		},
+	],
 };
